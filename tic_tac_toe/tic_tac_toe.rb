b5ec72e6f976992b0e653b3fc38dda63cc8b7bec
Jens Kristian Espevik <jenskristian@gmail.com>
2018-12-02 10:54:44 +0100

    add update board method

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -13,11 +13,41 @@ class Board
                   [ '3', '-', '-', '-']]
   end
 
+  def update_board(number, letter)
+
+    if !(number.to_i > 0 && number.to_i < 4) || 
+      letter != "A" || 
+      letter != "B" || 
+      letter != "C"
+      puts "error: incorrect input. Number 1-3 and Letter A, B or C"
+      return
+    end
+
+    # how many indexes to add when placing X/O
+    case letter
+      when "A" 
+        letter = 1
+      when "B"
+        letter = 2
+      when "C"
+        letter = 3
+    end
+
+    @board_array.map do |y|
+      y.map do |x|
+        if x == number
+          y[0 + letter] = "X"
+          return
+        end
+      end
+    end
+  end
+
   def print_board
     @board_array.each do |element|
       print "".rjust(5)
-      element.each do |y|
-        print y.ljust(2)
+      element.each do |x|
+        print x.ljust(2)
       end
       puts ""
     end
@@ -26,4 +56,11 @@ class Board
 end
 
 board = Board.new("p1", "p2")
+board.print_board
+puts "Number: "
+number = gets.chomp
+puts "Letter: "
+letter = gets.chomp.upcase
+puts number + letter
+board.update_board(number, letter)
 board.print_board
